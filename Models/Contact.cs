4a39b12d738eb7848df42341eb2ac4ddb2a38817
Tyler Redshaw <tjr5av@virginia.edu>
2018-10-16 20:12:08 -0400

    EF Scaffolding

---
+++ Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace react-test-app.Models
+{
+    public partial class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Models/userInformationContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace react-test-app.Models
+{
+    public partial class userInformationContext : DbContext
+    {
+        public userInformationContext()
+        {
+        }
+
+        public userInformationContext(DbContextOptions<userInformationContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contact> Contact { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("Server=127.0.0.1;Database=userInformation;UID=testUser;pwd=a12345;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>(entity =>
+            {
+                entity.ToTable("contact");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasColumnType("varchar(120)");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasColumnType("varchar(120)");
+            });
+        }
+    }
+}
